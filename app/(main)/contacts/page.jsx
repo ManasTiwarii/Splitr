55dbced2681bedcb4fc763fb1c0042d8d3bb635d
Manas Tiwari <96811126+ManasTiwarii@users.noreply.github.com>
2025-09-02 00:26:15 +0530

    Update page.jsx

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -12,6 +12,7 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { AvatarImage } from "@/components/ui/avatar";
 import CreateGroupModal from "./components/create-group-modal";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const ContactsPage = () => {
   const [isCreateGroupModelOpen, setIsCreateGroupModelOpen] = useState("false");
